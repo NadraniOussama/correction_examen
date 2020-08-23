0409567d71c0054bb1a89d9a28c2235f05949468
nadrani oussama <nadranisama@gmail.com>
2020-08-23 23:30:12 +0100

    hey

--- exam-2018/no-structur/haut.php
+++ exam-2018/no-structur/haut.php
@@ -1,3 +1,29 @@
+<?php 
+function afficherDate($lang) {
+/////La date sur le serveur
+$jours["AR"] = array("الأحد","الإثنين","الثلاثاء","الأربعاء","الخميس","الجمعة","السبت");
+$jours["FR"] = array("Dimanche","Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi");
+$jours["EN"] = array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
+
+$months["AR"]=["يناير ", "فبراير", "مارس ", "أبريل", "ماي ", "يونيو" , "يوليوز" , "غشت ", "شتنبر" ,"أكتوبر" , "نونبر" , "دجنبر"];
+
+$months["EN"]= ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+$months["FR"]= ["Janvier","Février","Mars", "Avril","Mai","juin","Juillet","Aôut","Septembre","Octobre","Novembre","Décembre"];
+
+
+$d = getdate();
+
+$wj = $d["wday"];
+$mj = $d["mday"];
+$m = $d["mon"];
+$y = $d["year"];
+
+$d= $jours[$lang][$wj]. " " . $mj . " " . $months[$lang][$m-1] . " " . $y;
+return $d;
+}
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
